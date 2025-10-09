3d57a5b2f3f98043ae42787a77297ae1b985b5d3
EstebanCarrilloG <estexav0996@gmail.com>
2025-10-09 12:31:03 -0500

    update myApp.js formating code

--- myApp.js
+++ myApp.js
@@ -1,55 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const helmet = require('helmet');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require("helmet");
 
 module.exports = app;
-const api = require('./server.js');
+const api = require("./server.js");
 app.use(helmet.hidePoweredBy());
 // setting "X-Frame-Options" to "DENY"
 app.use(
@@ -57,11 +11,11 @@ app.use(
     action: "deny",
   })
 );
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
